374bc2d0c1990fa29cd5b9ca4adb5307dc80a8a7
Matt Burton <burtonmatthewa@gmail.com>
2017-04-29 00:50:36 -0400

    Updated syntax highlighting to latest langums spec

--- src/extension.ts
+++ src/extension.ts
@@ -4,8 +4,5 @@ import CompletionProvider from './Providers/Completion/index';
 // this method is called when vs code is activated
 export function activate(context: vscode.ExtensionContext) 
 {
-
-	console.log('TRANSFORM AND ROLL OUT');
-
 	context.subscriptions.push(vscode.languages.registerCompletionItemProvider('langums', new CompletionProvider(context), '.', '\"'));
 }
